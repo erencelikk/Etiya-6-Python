b58385c123a48799d8aaf02306aa6f532e12731d
Eren <erencelik@gmail.com>
2023-04-06 14:49:03 +0300

    2. Gün Uygulama

---
+++ main.py
@@ -0,0 +1,21 @@
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final * 0.6) 
+
+# eğer final 40'dan küçükse kullanıcı kaldı
+# eğer ortalama 50'den küçükse kullanıcı kaldı
+# eğer vize finalin 2 katı ise kullanıcı kaldı
+# bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
+
+if final < 40 :
+    print("Kullanici kaldi")
+elif ortalama < 50:
+    print("Kullanici kaldi")
+elif vize * 2 == final:
+    print("Kullanici kaldi")
+else:
+    print("Geçtiniz!")
+    
+    
+    
+
